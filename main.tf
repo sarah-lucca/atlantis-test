d9f842e7c689035e7d262c0f49d08c6447154ba0
sarah97 <slucca@slucca-mac2.bit9.local>
2019-03-15 12:03:05 -0400

    atlantis test commit

---
+++ main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "atlantis_test_bucket" {
+  bucket = "slucca-atlantis-test"
+}
---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    region = "us-east-1"
+  }
+
+  required_version = "~> 0.11"
+}
+
+provider "aws" {
+  region  = "${var.region}"
+  version = "~> 1.3"
+}
